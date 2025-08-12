18dfd2c2b87b824b7f203287e7648ca4fcc10f81
[dyad] <huulong111@gmail.com>
2025-08-12 22:52:45 +0700

    [dyad] wrote 1 file(s)

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
-import { vi } from 'date-fns/locale';
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -31,7 +30,7 @@ export function DatePicker({ date, setDate }: DatePickerProps) {
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP", { locale: vi }) : <span>Chọn một ngày</span>}
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">

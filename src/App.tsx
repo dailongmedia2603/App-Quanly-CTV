59db0f598b122389dae0b4ff4678135127c460cb
[dyad] <huulong111@gmail.com>
2025-08-16 14:20:38 +0700

    [dyad] Sửa lỗi treo giao diện sau khi có thông báo - wrote 1 file(s), deleted 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -135,7 +134,6 @@ const App = () => {
       <TooltipProvider>
         <AuthProvider>
           <AppSettingsProvider>
-            <Toaster />
             <Sonner position="bottom-right" toastOptions={{ classNames: { success: "bg-brand-orange-light text-brand-orange border-orange-200", error: "bg-red-100 text-red-600 border-red-200", loading: "bg-brand-orange-light text-brand-orange border-orange-200" } }} />
             <BrowserRouter>
               <AppContent />
